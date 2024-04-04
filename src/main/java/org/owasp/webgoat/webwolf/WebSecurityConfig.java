bc724a3b32fe10226834d3be605aa8d3b113b1f4
aguadoeduardo <126874116+aguadoeduardo@users.noreply.github.com>
2024-04-04 18:14:13 +0200

    Actualización WebSecurityConfig.java
    Solución errores

--- src/main/java/org/owasp/webgoat/webwolf/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/webwolf/WebSecurityConfig.java
@@ -65,7 +65,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService).passwordEncoder(new bCryptPasswordEncoder());
+    auth.userDetailsService(userDetailsService).passwordEncoder(BCryptPasswordEncoder());
   }
 
   @Bean
